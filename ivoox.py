6bfda79599bbd41290ab7c411dd3532bca8a2107
tdc <daniel@thedogcompany.cl>
2023-11-20 23:33:06 -0300

    release 1.1

--- ivoox.py
+++ ivoox.py
@@ -3,55 +3,49 @@ import os
 
 import bs4
 import requests
+import re
 
 
-def new_bs(url):
+def new_bs(url: str):
     return bs4.BeautifulSoup(requests.get(url).content, "html5lib")
 
 
-def get_urls_page(soup: bs4.BeautifulSoup):
-    ans = []
+def get_urls_page(soup: bs4.BeautifulSoup) -> list[str]:
     rect = soup.find_all("p", "title-wrapper text-ellipsis-multiple")
-    for r in rect:
-        ans += [r.find('a').get('href')]
-    return ans
+    return [r.find('a').get('href') for r in rect]
 
 
-def hasnext(soup: bs4.BeautifulSoup):
+def has_next(soup: bs4.BeautifulSoup) -> str | None:
     new_url = None
-    pag = soup.find("ul", "pagination")
-    if not pag:
+    pagination = soup.find("ul", "pagination")
+    if not pagination:
         return new_url
-    pags = pag.find_all("li")
-    sig = pags[-1]
-    if sig.get("class") is None or "disabled" not in sig.get("class"):
-        new_url = sig.find('a').get('href')
+    pages = pagination.find_all("li")
+    next_page = pages[-1]
+    if next_page.get("class") is None or "disabled" not in next_page.get("class"):
+        new_url = next_page.find('a').get('href')
     return new_url
 
 
-def get_urls(root_url: str):
-    soup = new_bs(root_url)
+def get_urls(root_url: str) -> list[str]:
+    next_page = root_url
     ans = []
-    while 1:
-        ans += get_urls_page(soup)
-        next_page = hasnext(soup)
-        if not next_page:
-            break
+    while next_page is not None:
         soup = new_bs(next_page)
+        ans += get_urls_page(soup)
+        next_page = has_next(soup)
     return ans
 
 
 def get_download_url(url: str):
-    link = new_bs(url).find_all('script')
+    scripts = new_bs(url).find_all('script')
 
-    import re
-
-    bingo = re.compile(".downloadlink'\)")
+    bingo = re.compile(r".downloadlink'\)")
 
     def search():
-        for l in link:
-            if len(l.contents) > 0:
-                for r in l.contents[0].split('\n'):
+        for script in scripts:
+            if len(script.contents) > 0:
+                for r in script.contents[0].split('\n'):
                     if bingo.search(r):
                         return "https://www.ivoox.com/" + r.strip(' ')[25:-3]
 
@@ -60,15 +54,23 @@ def get_download_url(url: str):
 
 def get_download_urls(root):
     pages = get_urls(root)
-    return list(map(lambda x: get_download_url(x), pages))
+    return [get_download_url(x) for x in pages]
 
 
 def format_n(n):
     return ("000" + str(n))[-3:]
 
 
-if __name__ == '__main__':
-    # Lectura
+def download_link(_url: str, target_path: str):
+    print(_url)
+    req = requests.get(_url, stream=True, headers={'Referer': 'https://www.ivoox.com/'})
+    with open(target_path, 'wb') as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", type=str, help="url of audio")
     parser.add_argument("name", type=str, help="name of audio")
@@ -95,11 +97,10 @@ if __name__ == '__main__':
 
     print("Start Download")
     for i, _url in enumerate(links):
-        local_filename = os.path.join(download_path, f"{name}_{format_n(i+1)}.mp3")
-        print(_url)
-        req = requests.get(_url, stream=True)
-        with open(local_filename, 'wb') as f:
-            for chunk in req.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-        print(f"Download : {100*(i+1) / len(links):.2f}%")
+        local_filename = os.path.join(download_path, f"{name}_{format_n(i + 1)}.mp3")
+        download_link(_url, local_filename)
+        print(f"Download : {100 * (i + 1) / len(links):.2f}%")
+
+
+if __name__ == '__main__':
+    main()
